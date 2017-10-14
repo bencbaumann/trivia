93ec99745cf10b65658cad1cd80bf53615a215ba
Ben Baumann <ben@thisisbam.com>
2017-10-14 11:36:10 -0400

    push it up

--- assets/js/controllers/ui.js
+++ assets/js/controllers/ui.js
@@ -5,10 +5,18 @@ module.exports = {
         var h2 = document.createElement('h2');
         h2.innerHTML = question.question;
         div.appendChild(h2);
-        this.divQuestions.appendChild(div);
-        question.choices.forEach(function(element) {
-            
+        question.choices.forEach(function(choice) {
+            var radio = document.createElement('input');
+            radio.type = 'radio';
+            radio.id = choice;
+            radio.name = question.id;
+            var label = document.createElement('label');
+            label.for = choice;
+            label.innerHTML = choice;
+            div.appendChild(radio);
+            div.appendChild(label);
         });
+        this.divQuestions.appendChild(div);
         console.log(question);
     }
 }
