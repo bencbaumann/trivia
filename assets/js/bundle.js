c51ef41441a8c7a3dfd9b04be99cc6c02df6fd93
Ben Baumann <ben@thisisbam.com>
2017-10-14 16:57:44 -0400

    push it up

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -75,6 +75,7 @@ ui.displayQuestion(quiz.getQuestion());
 ui.nextA.addEventListener("click", quiz.position++, nextQuestion);
 
 function nextQuestion(){
+    console.log("Successful event capture");
     ui.displayQuestion(++quiz.position);
 }
 
--- assets/js/index.js
+++ assets/js/index.js
@@ -6,5 +6,6 @@ ui.displayQuestion(quiz.getQuestion());
 ui.nextA.addEventListener("click", quiz.position++, nextQuestion);
 
 function nextQuestion(){
+    console.log("Successful event capture");
     ui.displayQuestion(++quiz.position);
 }
