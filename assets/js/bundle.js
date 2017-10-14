6e88d0bae79ae0c7b13c203c424113035050a5ec
Ben Baumann <ben@thisisbam.com>
2017-10-14 11:29:53 -0400

    push it up

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -124,6 +124,13 @@ module.exports = {
     divQuestions : document.getElementById('questions'),
     displayQuestion(question){
         var div = document.createElement('div');
+        var h2 = document.createElement('h2');
+        h2.innerHTML = question.question;
+        div.appendChild(h2);
+        divQuestions.appendChild(div);
+        question.choices.forEach(function(element) {
+            
+        });
         console.log(question);
     }
 }
